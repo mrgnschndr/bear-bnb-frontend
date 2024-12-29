30f784b905c4390bd8e4e4bff6bd2372e90f8444
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-12-29 13:44:44 -0700

    created personal info container and set width to 50%

---
+++ src/Components/personal-info.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './personal-info.css';
+
+export default function PersonalInfo() {
+    return (
+        <div className="personal-info">
+            <h1 >Personal Info</h1>
+        </div>
+    );
+}
