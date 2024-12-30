f9b29b71cc9cd75fbad8da715530eeff89f7c752
krissyjames <kristina.ashley.cook@gmail.com>
2024-12-29 23:27:35 -0500

    styling of preferred name edit, preferred name, and adding both to main page

--- src/Components/personal-info.js
+++ src/Components/personal-info.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './personal-info.css';
 import LegalName from './legalName';
 import LegalNameEdit from './legalNameEdit';
+import PreferredName from './preferredName';
+import PreferredNameEdit from './preferredNameEdit';
 
 export default function PersonalInfo() {
     return (
@@ -9,6 +11,8 @@ export default function PersonalInfo() {
             <h1 >Personal Info</h1>
             <LegalName />
             <LegalNameEdit />
+            <PreferredName />
+            <PreferredNameEdit />
         </div>
     );
 }
--- src/Components/preferredNameEdit.js
+++ src/Components/preferredNameEdit.js
@@ -4,21 +4,21 @@ import { Box, TextField } from '@mui/material';
 
 export default function PreferredNameEdit() {
     return (
-        <div className="info-section">
-            <div>
-            <h3>Preferred name</h3>
-            <p>This is how your first name will appear to Host and guests. <a href="google.com">Learn more</a></p>
+        <div className="info-section-edit">
+            <div className="preferred-edit">
+                <div className="preferred-text">
+                    <h3>Preferred name</h3>
+                    <p>This is how your first name will appear to Host and guests. <a href="google.com">Learn more</a></p>
+                </div>
+                {/* Grabbed from text field mui */}
+                <TextField
+                    id="outlined-password-input"
+                    fullWidth label="Preferred name (optional)"
+                    input type="name"
+                    />
+                <button type="submit" className="save-btn">Save</button>
             </div>
-            <button>Cancel</button>
-            {/* Grabbed from text field mui */}
-            <form>
-            <TextField
-          id="outlined-password-input"
-          fullWidth label="Preferred name (optional)"
-          input type="name"
-        />
-            <button type="submit">Save</button>
-            </form>
+            <button className="cancel-btn">Cancel</button>
         </div>
     )
 }
