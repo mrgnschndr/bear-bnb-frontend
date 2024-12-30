b049fbe72cc668c85f67bad0622231d671a71bf5
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-12-30 11:46:08 -0700

    added toggle functionality to edit / cancel buttons

--- src/Components/personal-info.js
+++ src/Components/personal-info.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './personal-info.css';
 import LegalName from './legalName';
 import LegalNameEdit from './legalNameEdit';
@@ -6,11 +6,17 @@ import PreferredName from './preferredName';
 import PreferredNameEdit from './preferredNameEdit';
 
 export default function PersonalInfo() {
+    const [editLegalNameOpen, setEditLegalName] = useState(false);
+
+    function toggleEditMenu() {
+        setEditLegalName((prevState) => !prevState)
+    }
+
     return (
         <div className="personal-info">
             <h1 >Personal Info</h1>
-            <LegalName />
-            <LegalNameEdit />
+            {!editLegalNameOpen && <LegalName toggleEditMenu={toggleEditMenu}/> }
+            {editLegalNameOpen && <LegalNameEdit toggleEditMenu={toggleEditMenu}/> }
             <PreferredName />
             <PreferredNameEdit />
         </div>
