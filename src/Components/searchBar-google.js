c734c5e05219084a7a913206f1ddaae27a9aafda
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2025-01-11 13:01:04 -0700

    removed console log of api key

--- src/Components/searchBar-google.js
+++ src/Components/searchBar-google.js
@@ -11,7 +11,7 @@ import { debounce } from '@mui/material/utils';
 // This key was created specifically for the demo in mui.com.
 // You need to create a new one for your application.
 let apiKey=process.env.REACT_APP_GOOGLE_API_KEY;
-console.log("key", apiKey);
+// console.log("key", apiKey);
 
 function loadScript(src, position, id) {
   if (!position) {
