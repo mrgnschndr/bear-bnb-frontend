f2a6709a8d84000854f6b322958cfde526fb41b5
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2025-01-11 17:28:10 -0700

    changed brackets to quotes on state declaration

--- src/context/ListingContext.js
+++ src/context/ListingContext.js
@@ -7,7 +7,7 @@ export const ListingContext = createContext();
 // Define the Listing Provider component to manage the listing fetching and login simulation logic
 export function ListingProvider({ children }) {
   // State variable to hold the list of listings // Jess has this as null in her video
-  const [listingArray, setListingArray] = useState([]);
+  const [listingArray, setListingArray] = useState("");
   // State variable to track the loading state (for example, during data fetching)
   const [loading, setLoading] = useState(true);
 
