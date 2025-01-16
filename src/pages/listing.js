78f3e2695f04828dedb49de46261cd903405db61
krissyjames <kristina.ashley.cook@gmail.com>
2025-01-16 17:23:27 -0500

    completion of listing extras block

--- src/pages/listing.js
+++ src/pages/listing.js
@@ -7,6 +7,9 @@ import StarIcon from '@mui/icons-material/Star';
 import Stack from '@mui/material/Stack';
 import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
 import Rating from '@mui/material/Rating';
+import DoorFrontOutlinedIcon from '@mui/icons-material/DoorFrontOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import DateRangeOutlinedIcon from '@mui/icons-material/DateRangeOutlined';
 
 import { SpaceBar } from '@mui/icons-material';
 import ReserveCard from '../Components/ReserveCard';
@@ -36,6 +39,16 @@ export default function Listing() {
     useEffect(() => {
         fetchListing();
     }, [listingId]);
+
+// Date variables for cancellation date 14 days from current date
+    // variable for current date
+    let date = new Date();
+    //setDate sets the day of the month for the current date. In this instance instead of a number we are using getDate (returns the current day of the month and using here to make it dynamic) plus 14. This is to add two weeks to the current date to make the cancellation date correct.
+    let datePlus = date.setDate(date.getDate()+14);
+    // this gives us the date form of the date we created above so we can use the variable below
+    let cancelDay = new Date(datePlus);
+    // now we use the cancel date and turn it into month and day only.
+    let cancelDayText = cancelDay.toLocaleDateString ('en-us', {month:"long", day:"numeric"})
 
 
     // Render the listing details or error message
@@ -93,6 +106,35 @@ export default function Listing() {
                             : `${Number(new Date().getFullYear()) - Number((listing.date_hosted).slice(0, 4))} year(s) hosting`}</p>
                     </div>
                     <hr></hr>
+                    <div className="space-extras">
+                        {(listing.is_self_checkin === true) ? (
+                            <div className="self-check-in">
+                                <DoorFrontOutlinedIcon /> 
+                                <div className="extras-block">
+                                <h3>Self check-in</h3>
+                                <p>Check yourself in with the smartlock.</p>
+                                </div>
+                            </div>
+                        ) : (null)}
+                        {(listing.is_peaceful === true) ? (
+                            <div className="peaceful">
+                                <LocationOnOutlinedIcon /> 
+                                <div className="extras-block">
+                                <h3>Peace and quiet</h3>
+                                <p>Guests say this home is in a quiet area.</p>
+                                </div>
+                            </div>
+                        ) : (null)}
+                        <div className="cancel-date">
+                            <DateRangeOutlinedIcon />
+                            <div className="extras-block">
+                            <h3>Free cancellation before {cancelDayText}</h3>
+                            <p>Get a full refund if you change your mind.</p>
+                            </div>
+
+                        </div>
+                        <hr></hr>
+                    </div>
                     <p>{listing.space_description}</p>
                     {/* <p><strong>Address:</strong> {listing.listing_address}</p>
                     <p><strong>City:</strong> {listing.listing_city}, {listing.listing_state}</p>
